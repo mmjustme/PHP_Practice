ab4aa00542616f37c0ccbfc169eda9c4066a1cec
mmjustme <infooneuser@gmail.com>
2024-09-07 19:50:48 +0300

    Lesson_05: 1. Deleted old comments

--- public/index.php
+++ public/index.php
@@ -11,31 +11,7 @@ define("PATH", 'http://localhost/PHP_Practice');
 
 require CORE . "/funcs.php";
 
-// маємо пробоему коли в строку долаємо GET параметри, тому робимо наступним чином
-// parse_url розбиває нашу строку на такі параметри
-// var_dump(parse_url('http://localhost/PHP_Practice/about?id=1'));
-// array(4) { ["scheme"]=> string(4) "http" 
-// ["host"]=> string(9) "localhost" 
-// ["path"]=> string(19) "/PHP_Practice/about" 
-// ["query"]=> string(4) "id=1" 
-// }
-// тим самим відділяє основну строку і інші параметри
-
-// Означає ми одразу беремо частину шляху яка наз path а це /PHP_Practice/about
-// а тоді вже забираємо "/" в кінці через trim
-// і ми знову отримуємо "PHP_Practice/about" але тепер можемо вписувати get параметри в строку
 $uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/"); // PHP_Practice
-// dd($uri);
-
-// також GET параметри втрачаються оск необхідно дадати налаштування htaccess такі як [L,QSA]
-// що озн додати в кінець усі усі заппити в адресну строку
-// dump($_GET);
-// array(2) {
-// ["about"]=>
-// string(0) ""
-// ["id"]=>
-// string(1) "1"
-//   }
 
 if ($uri === "PHP_Practice") {
     require CONTROLLERS . '/index.php';
