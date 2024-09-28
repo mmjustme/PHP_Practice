ddd1b62bd20461a13d0c8e2ca856ad1102cb84dd
mmjustme <infooneuser@gmail.com>
2024-09-28 19:16:56 +0300

    Lesson_14: 1. little update

--- app/controllers/posts/show.php
+++ app/controllers/posts/show.php
@@ -1,7 +1,11 @@
 <?php
 global $db;
 
-$id = (int) $_GET["id"] ?? 0;
+/** 
+ * @var Db $db 
+ **/
+
+$id = $_GET["id"] ?? 0;
 
 # захист query від SQL інєкції
 $post = $db->query("SELECT * FROM posts WHERE id = :id LIMIT 1", [":id" => $id])->findOrFail();
