8da486796589dfa76063a33bef7825e835b6a5f2
mmjustme <infooneuser@gmail.com>
2024-09-28 17:34:14 +0300

    Lesson_14: 1. Add post get delete methods

--- vendor/myfrm/core/classes/Router.php
+++ vendor/myfrm/core/classes/Router.php
@@ -11,4 +11,29 @@ class Router
         $this->uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/");
         $this->method = $_SERVER['REQUEST_METHOD'];
     }
+
+    # function helper avoid excessive code
+    public function add($uri, $controller, $method)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'methods' => $method,
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->add($uri, $controller, "GET");
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->add($uri, $controller, "POST");
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->add($uri, $controller, "DELETE");
+    }
 }
