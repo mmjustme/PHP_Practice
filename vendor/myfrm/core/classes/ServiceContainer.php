4be28aef56db21e7d9fcea4dd8e94e883e64545c
mmjustme <infooneuser@gmail.com>
2024-09-29 13:08:16 +0300

    Lesson_16: 1. Service container

---
+++ vendor/myfrm/core/classes/ServiceContainer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace myfrm;
+class ServiceContainer
+{
+    # контейнер для зберіганні сервісів
+    protected $services = [];
+
+    public function setService($service, $func)
+    {
+        # тобто ми в масив $services по ключам $service кладемо колбек функції
+        $this->services[$service] = $func;
+    }
+
+    public function getService($service)
+    {
+        # перевірка чи є в нас сервіс який викликається
+        if (!isset($thi->services[$service])) {
+            throw new \Exception("Not found service {$service}");
+        }
+        # викликаємо наш сервіс і запускаємо його через call_user_func
+        return call_user_func($this->services[$service]);
+    }
+}
