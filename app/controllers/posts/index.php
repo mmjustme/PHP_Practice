8948ccbec2fbbff4fd3b03aa1d789c965ccc7a65
mmjustme <infooneuser@gmail.com>
2024-10-26 17:33:23 +0300

    Finished class Pagination.php

--- app/controllers/posts/index.php
+++ app/controllers/posts/index.php
@@ -6,27 +6,16 @@ use myfrm\Db;
 $title = 'MY BLOG :: HOME';
 
 $page = $_GET['page'] ?? 1;
-$per_page = 2;
+$per_page = 10;
 # поверне к-ть записів відповідно к-ть сторінок вирахуємо
 $total = db()->query("SELECT COUNT(*) FROM posts")->getColumn();
 $pagination = new \myfrm\Pagination((int)$page, $per_page, $total);
 
-print_arr($pagination);
 $start = $pagination->getStart();
 
-# при виклику класу через echo, під капотом php шукає метод класу toString
-# тому ми можемо видалити метод і створити в класі новий метод toString де і викличемо getHtml()
-# таким чином залишається чистий запис echo $pagination; замість echo $pagination->getHtml();
-echo $pagination;
-
-die();
-# тепер db() підтягує з функцій
 $posts = db()->query("SELECT * FROM posts ORDER BY id DESC LIMIT $start, $per_page")->findAll();
-// dd($posts);
 
-# також можна викоикати базу через App
 $db2 = App::get(Db::class);
 $recent_posts = $db2->query("SELECT * FROM posts ORDER BY id DESC LIMIT 3")->findAll();
-// dd($recent_posts[0]["id"]);
 
 require_once VIEWS . "/posts/index.tpl.php";
--- app/views/posts/index.tpl.php
+++ app/views/posts/index.tpl.php
@@ -1,34 +1,35 @@
 <?php require_once "../app/views/incs/header.php" ?>
 
-<main class="main py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <?php foreach ($posts as $post): ?>
-                    <div class="card mb-3">
+	<main class="main py-3">
+		<div class="container">
+			<div class="row">
+        <?= $pagination ?>
+				<div class="col-md-8">
+          <?php foreach ($posts as $post): ?>
+						<div class="card mb-3">
 
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <a href="posts?id=<?= $post['id'] ?>">
-                                    <?= h($post['title']); ?>
-                                </a>
+							<div class="card-body">
+								<h5 class="card-title">
+									<a href="posts?id=<?= $post['id'] ?>">
+                    <?= h($post['title']); ?>
+									</a>
 
-                            </h5>
-                            <p class="card-text">
-                                <?= $post['excerpt']; ?>
-                            </p>
-                            <a href="posts?id=<?= $post['id'] ?>">Go somewhere</a>
-                        </div>
+								</h5>
+								<p class="card-text">
+                  <?= $post['excerpt']; ?>
+								</p>
+								<a href="posts?id=<?= $post['id'] ?>">Go somewhere</a>
+							</div>
 
-                    </div>
-                <?php endforeach ?>
+						</div>
+          <?php endforeach ?>
+          <?= $pagination ?>
 
+				</div>
+        <?php require_once "../app/views/incs/sidebar.php" ?>
+			</div>
+		</div>
 
-            </div>
-            <?php require_once "../app/views/incs/sidebar.php" ?>
-        </div>
-    </div>
-
-</main>
+	</main>
 
 <?php require_once "../app/views/incs/footer.php" ?>
