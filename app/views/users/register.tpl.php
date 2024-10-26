647b5ced05dc6b1da0317dfec42000f432591279
mmjustme <infooneuser@gmail.com>
2024-10-26 18:41:31 +0300

    Lesson_22: Register user. 1. Small width corrections for form has been added. 2. Functionality for creating users have been added.

--- app/views/users/register.tpl.php
+++ app/views/users/register.tpl.php
@@ -4,7 +4,7 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12">
+				<div class="col-md-4 m-auto">
 					<h1>Register Page</h1>
 					<form action="/register" method="post">
 
