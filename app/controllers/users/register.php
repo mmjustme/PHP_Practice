2cc85d769478a66def031fc1f5305897f7c793f5
mmjustme <infooneuser@gmail.com>
2024-11-09 14:32:57 +0200

    Lesson_24: Files 1. Updated register.php. Added file check step.

--- app/controllers/users/register.php
+++ app/controllers/users/register.php
@@ -4,12 +4,48 @@ $title = 'My Blog :: Register';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-  $data = load(['name', 'email', 'password', 'avatar']);
+  $data = load(['name', 'email', 'password']);
   $validator = new \myfrm\Validator();
 # data from form
-  dump($_POST);
+//  dump($_POST);
 # data with files
-  dump($_FILES);
+//  dump($_FILES);
+
+
+  # перевірка на файл, в масиві про файл наявне поле error
+  # значення 4 - помилку, файл не завантажувався, 0 - завантажувався
+  # тому первірка чи взагалі є файл і чи зі ствтусом 0
+  if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] === 0) {
+    # додаємо файл в дані з форми
+    $data['avatar'] = $_FILES['avatar'];
+  } else {
+    $data['avatar'] = [];
+  }
+//  array(4) {
+//    ["name"]=>
+//  string(14) "Russell Campos"
+//    ["email"]=>
+//  string(20) "vepiw@mailinator.com"
+//    ["password"]=>
+//  string(9) "Pa$$w0rd!"
+//    ["avatar"]=>
+//  array(6) {
+//      ["name"]=>
+//    string(42) "scenic_view_sunset-wallpaper-1920x1280.jpg"
+//      ["full_path"]=>
+//    string(42) "scenic_view_sunset-wallpaper-1920x1280.jpg"
+//      ["type"]=>
+//    string(10) "image/jpeg"
+//      ["tmp_name"]=>
+//    string(24) "C:\xampp\tmp\php6097.tmp"
+//      ["error"]=>
+//    int(0)
+//    ["size"]=>
+//    int(395977)
+//  }
+//}
+
+
 # data after fn load with field avatar
   dd($data);
 
