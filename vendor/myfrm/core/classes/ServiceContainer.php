8e2c7010b4a8e2860c0043c8648aa482929c3aec
mmjustme <infooneuser@gmail.com>
2024-11-09 13:50:20 +0200

    Small refactor of styles.

--- vendor/myfrm/core/classes/ServiceContainer.php
+++ vendor/myfrm/core/classes/ServiceContainer.php
@@ -3,22 +3,22 @@
 namespace myfrm;
 class ServiceContainer
 {
-    # контейнер для зберіганні сервісів
-    protected $services = [];
+  # контейнер для зберіганні сервісів
+  protected $services = [];
 
-    public function setService($service, $func)
-    {
-        # тобто ми в масив $services по ключам $service кладемо колбек функції
-        $this->services[$service] = $func;
-    }
+  public function setService($service, $func)
+  {
+    # тобто ми в масив $services по ключам $service кладемо колбек функції
+    $this->services[$service] = $func;
+  }
 
-    public function getService($service)
-    {
-        # перевірка чи є в нас сервіс який викликається
-        if (!isset($this->services[$service])) {
-            throw new \Exception("Not found service {$service}");
-        }
-        # викликаємо наш сервіс і запускаємо його через call_user_func
-        return call_user_func($this->services[$service]);
+  public function getService($service)
+  {
+    # перевірка чи є в нас сервіс який викликається
+    if (!isset($this->services[$service])) {
+      throw new \Exception("Not found service {$service}");
     }
+    # викликаємо наш сервіс і запускаємо його через call_user_func
+    return call_user_func($this->services[$service]);
+  }
 }
